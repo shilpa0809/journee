d19ae7c83611bdcc65ab504f0718cdfbce950766
Shilpa <shilpa.k.mishra@gmail.com>
2023-11-19 13:18:36 +0100

    Fixing focus issue | ItemList improvised to fix the bug

--- src/components/ItemList.tsx
+++ src/components/ItemList.tsx
@@ -1,16 +1,16 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import styled from "styled-components";
 import { addTodo, editTodo } from "../todo/todoAC";
 import { useAppDispatch, useAppSelector } from "../types";
 
 const List = styled.ul`
   width: 100%;
+  box-sizing: border-box;
   display: flex;
   flex-direction: column;
-  box-sizing: border-box;
 `;
 
-const Item = styled.li`
+const ItemContainer = styled.li`
   width: 100%;
   margin: 4px 0;
   display: flex;
@@ -25,29 +25,47 @@ const ItemCheckbox = styled.input.attrs({
   type: "checkbox",
 })``;
 
+const Item: React.FC<{ item: any | null }> = ({ item }) => {
+  const dispatch = useAppDispatch();
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    // Set focus on the input element when the component mounts
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, []);
+
+  const handleChange = (value: string) => {
+    if (item) {
+      dispatch(editTodo(item.id, value));
+    } else {
+      dispatch(addTodo(value));
+    }
+  };
+
+  return (
+    <ItemContainer>
+      <ItemCheckbox />
+      <ItemInput
+        type="text"
+        value={item ? item.text : ""}
+        onChange={(e) => handleChange(e.target.value)}
+        ref={inputRef} // Assign the ref to the input element
+      />
+    </ItemContainer>
+  );
+};
+
 const ItemList: React.FC = () => {
   const items = useAppSelector((s) => s.todos.items);
-  const dispatch = useAppDispatch();
+
   return (
     <List>
       {items.map((item) => (
-        <Item key={item.id}>
-          <ItemCheckbox />
-          <ItemInput
-            type="text"
-            value={item.text}
-            onChange={(e) => dispatch(editTodo(item.id, e.target.value))}
-          />
-        </Item>
+        <Item key={item.id} item={item} />
       ))}
-      <Item key="new">
-        <ItemCheckbox />
-        <ItemInput
-          type="text"
-          value=""
-          onChange={(e) => dispatch(addTodo(e.target.value))}
-        />
-      </Item>
+      <Item key="new" item={null} />
     </List>
   );
 };
